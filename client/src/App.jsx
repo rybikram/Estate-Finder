d52c6caaaffeac5eb1913ea4b2ab5dfe102b870b
rybikram <bikram.roy507@gmail.com>
2024-02-06 16:18:30 +0530

    updated

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignIn from './Pages/SignIn'
+import SignUp from './Pages/SignUp'
+import About from './Pages/About'
+import Profile from './Pages/Profile'
+import Header from './components/Header'
+
+
+function App() {
+  return <BrowserRouter>
+  <Header />
+  <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/sign-in' element={<SignIn />} />
+      <Route path='/sign-up' element={<SignUp />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/profile' element={<Profile />} />
+  </Routes>
+ </BrowserRouter>
+}
+
+export default App
