e619c74c8590640741d5299833cc4991a88fdd6f
rybikram <bikram.roy507@gmail.com>
2024-03-02 19:40:03 +0530

    Create search API route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createListing, deleteListing, updateListing, getListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing, getListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router()
@@ -9,5 +9,6 @@ router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)  //here this is the the delete of perticular listing from the listings
 router.post('/update/:id', verifyToken, updateListing)
 router.get('/get/:id', getListing)            //to get information of perticular listing details
+router.get('/get', getListings)                      //For search functionality
 
 export default router
